1b138b24fe02b976780c87c6d872a6d0ebc69605
Michael Kidd <G00236920@gmit.ie>
2018-02-20 00:42:41 +0000

    Pi now transmits accelerometer data to unity

--- RaspberryPiFishingProject/FishPi/MainPage.xaml.cs
+++ RaspberryPiFishingProject/FishPi/MainPage.xaml.cs
@@ -1,65 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using Windows.Devices.Sensors;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Emmellsoft.IoT.Rpi.SenseHat;
+using Windows.UI.Xaml;
+using System;
+using System.Threading.Tasks;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace FishPi
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
+
+        private ISenseHat _senseHat { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
-            Start();
+            Loaded += Start;
         }
 
-        static void Start()
+        private async void Start(object sender, RoutedEventArgs e)
         {
+            //get our sense hat
+            _senseHat = await GetSenseHatAsync();
 
-            Accelerometer _accerometer = Accelerometer.GetDefault();
-
+            //udp client on port 5000
             UdpClient udpClient = new UdpClient(5000);
 
+            //Ip address and port number of our unity build
             string ip = "192.168.0.5";
             int port = 5000;
 
+            //our endpoint
             IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ip), port);
 
+            //UDP socket
             Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
             while (true)
             {
+                //Update the data from the IMU sensor
+                _senseHat.Sensors.ImuSensor.Update();
 
-                AccelerometerReading reading = _accerometer.GetCurrentReading();
+                //If there is an acceleration value
+                if (_senseHat.Sensors.Acceleration.HasValue)
+                {
+                    //The data we will send over the network
+                    string data = _senseHat.Sensors.Acceleration.Value.X.ToString()+" "
+                        + _senseHat.Sensors.Acceleration.Value.Y.ToString()+" "
+                        +_senseHat.Sensors.Acceleration.Value.Z.ToString();
 
-                string data = "x: " +reading.AccelerationX +"/n"
-                             +"Y: " +reading.AccelerationY +"/n"
-                             +"Z: " +reading.AccelerationZ;
+                    //Byte array to package our data
+                    byte[] sendBytes = Encoding.ASCII.GetBytes(data);
+                    //Send the data using a udpclient with our endpoint
+                    client.SendTo(sendBytes, ep);
 
-                byte[] sendBytes = Encoding.ASCII.GetBytes(data);
-                client.SendTo(sendBytes, ep);
+                }
+                //if there is no acceleration value
+                else
+                {
+                    //Wait and try again
+                    Task.Delay(20).Wait();
+
+                }
 
             }
 
         }
+
+        private async Task<ISenseHat> GetSenseHatAsync() {
+
+            Task<ISenseHat> task = Task.Run(async() => 
+            {
+
+                return await SenseHatFactory.GetSenseHat();
+
+            });
+
+            task.Wait();
+
+            return await task;
+
+        }
     }
+
 }
